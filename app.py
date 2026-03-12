f1c5a3e6e9ed6d589e2c932842c9a818f0b62f4c
Rahman-Shaon <rahmanshaon760@gmail.com>
2026-03-13 00:33:05 +0600

    Initial Flask project setup

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+import mysql.connector
+
+app = Flask(__name__)
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "MySQLhello#1",
+    database = "flask_shop"
+)
+
+cursor = db.cursor()
+
+@app.route("/")
+def home():
+    return render_template("home.html", name = "Shaon")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+@app.route("/submit", methods = ["POST"])
+def submit ():
+    username = request.form["username"]
+
+    sql = "INSERT INTO users (username) VALUES (%s)"
+    val = (username,)
+    cursor.execute(sql, val)
+    db.commit()
+
+    return f"Hello {username}, saved to database!"
+
+
+@app.route("/users")
+def users():
+
+    sql = "SELECT * FROM users"
+    cursor.execute(sql)
+
+    all_users = cursor.fetchall()
+
+    return render_template("users.html", users=all_users)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
